b18dbb92720bb9048d76f6622df55232bda95508
Michael <95030896+MxkeCoding@users.noreply.github.com>
2023-09-13 23:13:32 +0800

    Add files via upload

---
+++ Introduction/Introduction.py
@@ -0,0 +1,19 @@
+import sys
+
+
+
+print("I'm a duck")
+
+print("       _.-.                    ")
+print("  __.-' ,  \                   ")
+print(" '--'-'._   \                  ")
+print("         '.  \                 ")
+print("          )-- \ __.--._        ")
+print("         /   .'        '--.    ")
+print("        .               _/-._  ")
+print("        :       ____._/   _-'  ")
+print("         '._          _.'-'    ")
+print("            '-._    _.'        ")
+print("                \_|/           ")
+print("               __|||           ")
+print("               >__/'           ")

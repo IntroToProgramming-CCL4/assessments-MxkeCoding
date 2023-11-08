be17b1884c97bf7d35241406e505cab4d9bb99b6
Michael <95030896+MxkeCoding@users.noreply.github.com>
2023-11-08 23:17:37 +0800

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 5(Favorite fruit).py
@@ -0,0 +1,18 @@
+#list of favorite fruits
+favorite_fruits = ["Tomatoes", "mangoes", "pineapple", "blueberries", "bananas" ]
+
+#it displays if the list has the given fruits
+if "Tomatoes" in favorite_fruits:
+    print("You must love tomatoes")
+    
+if "mangoes" in favorite_fruits:
+    print("You must love mangoes")
+
+if "pineapple" in favorite_fruits:
+    print("You must love pineapple")
+    
+if "blueberries" in favorite_fruits:
+    print("You must love blueberries")
+
+if "bananas" in favorite_fruits:
+    print("You must love bananas")

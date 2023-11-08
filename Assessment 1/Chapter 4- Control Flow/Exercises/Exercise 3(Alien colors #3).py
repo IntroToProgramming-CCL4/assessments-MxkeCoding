e357b04a2f3e1fef2232877d07792b5e45975433
Michael <95030896+MxkeCoding@users.noreply.github.com>
2023-11-08 23:16:41 +0800

    Update Exercise 3(Alien colors #3).py

--- Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 3(Alien colors #3).py
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 3(Alien colors #3).py
@@ -1,9 +1,12 @@
+#tells what color is the alien
 alien_color1 = "yellow"
 alien_color2 = "green"
 alien_color3 = "red"
 
+#lets the user type what color is the alien
 input_color = input("Type what color is the alien: ")
 
+#Displays how many points users get depending on the color they input
 if input_color == alien_color1:
     print("You just earned 5 points!")
 elif input_color == alien_color2:

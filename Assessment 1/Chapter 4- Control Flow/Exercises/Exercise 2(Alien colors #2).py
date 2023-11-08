822253fee81e67d5c6ffa51856aa285ad5b71cf9
Michael <95030896+MxkeCoding@users.noreply.github.com>
2023-11-08 23:16:11 +0800

    Update Exercise 2(Alien colors #2).py

--- Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 2(Alien colors #2).py
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise 2(Alien colors #2).py
@@ -1,8 +1,12 @@
+#tells what color is the alien
 alien_color = "yellow"
 
+#lets the user type what color is the alien
 input_color = input("Type what color is the alien: ")
 
+#if user types correct color, it displays that they earned 5 points
 if input_color == alien_color:
     print("You just earned 5 points!")
+#if they did not type the correct color, it displays that they earned 10 points 
 else:
     print("You just earned 10 points!")
